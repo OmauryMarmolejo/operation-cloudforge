8a330d7b3510c31d9afbce55bc00da21e811f58a
Omaury Marmolejo <omaury@tango.io>
2023-11-21 17:44:23 -0600

    Add aim role

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,61 @@
+resource "aws_key_pair" "ecs" {
+  key_name   = "ec2ecsglog"
+  public_key = tls_private_key.ecs.public_key_openssh
+}
+
+resource "tls_private_key" "ecs" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_iam_role" "ecs_instance_role" {
+  name = "ecs-instance-role"
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "",
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "ec2.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      },
+      {
+        "Sid" : "",
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "ecs-tasks.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_instance_role" {
+  role       = aws_iam_role.ecs_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
+  role       = aws_iam_role.ecs_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_instance_profile" "ecs_instance_profile" {
+  name = "ecs-instance-profile"
+  role = aws_iam_role.ecs_instance_role.name
+}
 resource "aws_launch_template" "ecs_lt" {
   name_prefix   = "ecs-template"
-  image_id      = "ami-062c116e449466e7f"
+  image_id      = "ami-093467ec28ae4fe03"
   instance_type = "t3.micro"
 
-  key_name               = "ec2ecsglog"
+  key_name               = aws_key_pair.ecs.key_name
   vpc_security_group_ids = [aws_security_group.security_group.id]
   iam_instance_profile {
-    name = "ecsInstanceRole"
+    name = aws_iam_instance_profile.ecs_instance_profile.name
   }
 
   block_device_mappings {
@@ -79,3 +128,6 @@ resource "aws_lb_target_group" "ecs_tg" {
     path = "/"
   }
 }
+
+
+
